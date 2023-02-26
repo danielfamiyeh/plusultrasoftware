a7cf1867b46ce3c6ccd45a6d29f0eccf1672498e
danielfamiyeh <daniel.kwaku.famiyeh@gmail.com>
2023-02-26 17:31:58 +0000

    Add border to bottom of header

--- src/components/landing/Header.tsx
+++ src/components/landing/Header.tsx
@@ -12,7 +12,7 @@ const Header = () => {
     containerElem?.scrollIntoView({ behavior: 'smooth' });
   };
   return (
-    <header className="fixed flex px-4 bg-[#4C4848] w-screen h-[48px] items-center justify-between z-10">
+    <header className="fixed flex px-4 bg-[#4C4848] w-screen h-[48px] items-center justify-between z-10 border-b-2 border-b-[rgba(128,128,128,0.7)]">
       <Image src={logo} height={16} alt="Logo" className="self-center" />
       <div className="hidden md:flex">
         {headerTitles.map((title) => (
